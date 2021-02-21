033a7a2662707d481704e0e5032143f76c0a5645
Robert <guder.robert@gmail.com>
2021-02-21 17:17:48 +0100

    Make points prediction based on trend of last three races

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ dir_path = "C:\\Users\\Dell-Laptop\\PycharmProjects\\ECyclyAutStats\\results\\"
 
 class Rider:
     total_races = 9
-    races_happened = 6
+    races_happened = 7  # TODO replace by count of result files
     races_counting = 7
 
     def __init__(self, name, surname, cat):
@@ -19,15 +19,24 @@ class Rider:
         self.avg_points = 0
         self.races_participated = 0
         self.predicted_points = 0
+        self.trend = 0
 
     def predict_points(self):
         races_remain = self.total_races - self.races_happened
-        races_missed = self.races_happened - self.races_participated
-        if (races_missed >= 2):
-            self.predicted_points = self.overall_points + races_remain * self.avg_points
+        # less/equal than races counting -> no results will be scratched
+        if (self.races_counting - self.races_participated) >= races_remain:
+            self.predicted_points = self.overall_points + races_remain * self.trend
+        # one can chose, which result should count
         else:
-            self.predicted_points = self.overall_points + (
-                        self.races_counting - self.races_participated) * self.avg_points
+            cpy_point_list_sorted_desc = sorted(self.points, reverse=True)
+            cpy_list_len = len(cpy_point_list_sorted_desc)
+            nr_of_possible_scratches = races_remain - (self.races_counting - self.races_participated)
+            # replace scratch results in list
+            for i in range(cpy_list_len - nr_of_possible_scratches, cpy_list_len):
+                cpy_point_list_sorted_desc[i - 1] = self.trend
+            scratch_list_pts = sum(cpy_point_list_sorted_desc)
+            self.predicted_points = scratch_list_pts + (
+                    self.races_counting - self.races_participated) * self.trend
 
     def add_result(self, val):
         int_val = int(val)
@@ -36,6 +45,10 @@ class Rider:
         self.avg_points = self.overall_points / len(self.points)
         self.races_participated = len(self.points)
         self.predict_points()
+        if len(self.points) < 3:
+            self.trend = self.avg_points
+        else:
+            self.trend = sum(self.points[-3:]) / 3
 
     def get_points_total(self):
         result = 0
@@ -45,6 +58,11 @@ class Rider:
 
     def is_equal(self, name, surname, cat):
         return (self.name == name) and (self.surname == surname) and (self.cat == cat)
+
+    def print_rider_stats(self):
+        print('{} {}'.format(self.name, self.surname))
+        for event in self.points:
+            print(event)
 
 
 def get_project_root() -> Path:
@@ -65,12 +83,12 @@ def read_results(rider_data):
             del lines[0:6]
             for line in lines:
                 words = line.split(" ")
-                if (words[0] != "DSQ" and words[0] != "DNF" and words[0] != "*"):
+                if words[0] != "DSQ" and words[0] != "DNF" and words[0] != "*":
                     rider_found = False
-                    # TODO add correctnesscheck
+                    # TODO add correctness check
                     name = words[2]
                     surname = words[3]
-                    if (len(words[len(words) - 2].split(".")) == 2):
+                    if len(words[len(words) - 2].split(".")) == 2:
                         points_for_race = words[len(words) - 1]
                     else:
                         points_for_race = words[len(words) - 2]
@@ -80,7 +98,7 @@ def read_results(rider_data):
                             rider.add_result(points_for_race)
                             rider_found = True
                             break
-                    if (not rider_found):
+                    if not rider_found:
                         new_rider = Rider(name, surname, cat)
                         new_rider.add_result(points_for_race)
                         rider_data.append(new_rider)
@@ -92,15 +110,22 @@ def read_results(rider_data):
 def print_results(riders, cat):
     riders.sort(key=lambda x: x.predicted_points, reverse=True)
     place = 1
+    print('Pos. Name \t\t races \t predicted (current || avg || trend)')
     for rider in riders:
-        if (rider.cat == cat):
-            print('{} {} \t\t {} \t {} ({} || {})'.format(place, rider.name, rider.races_participated,
-                                                          rider.predicted_points, rider.overall_points,
-                                                          rider.avg_points))
+        if rider.cat == cat:
+            print('{} {} \t\t {} \t {} ({} || {} || {})'.format(place, rider.name, rider.races_participated,
+                                                                rider.predicted_points, rider.overall_points,
+                                                                rider.avg_points, rider.trend))
             place += 1
 
 
+def print_rider(rider_data, name, surname, cat):
+    for rider in rider_data:
+        if rider.is_equal(name, surname, cat):
+            rider.print_rider_stats()
+
+
 if __name__ == '__main__':
-    rider_data = []
-    read_results(rider_data)
-    print_results(rider_data, "BIKECARD")
+    data = []
+    read_results(data)
+    print_results(data, "BIKECARD")
